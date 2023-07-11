be7ad9d20c5593fe1549295294be9871a0e8d6c5
Avinash Rapolu <94101850+Avinash6643@users.noreply.github.com>
2023-07-11 12:32:45 +0800

    Update ReadXMLFile.java

--- ReadXMLFile.java
+++ ReadXMLFile.java
@@ -13,53 +13,48 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ReadXMLFile
-{
-	public static void main(String[] args)
-	{
-		String location = "<add location>";
-		try
-		{
-			//  for two or more files uncomment and add the files based on time <New to Old>
-			
-			List<File> files = new ArrayList<>();
-//creating a constructor of file class and parsing an XML file
-			File file1 = new File(location+"file1.xml");
-			files.add(file1);
-			File file2 = new File(location+"file2.xml");
-			files.add(file2);
-			File file3 = new File(location+"file3.xml");
-			files.add(file3);
-			File file4 = new File(location+"file4.xml");
-			files.add(file4);
-			File file5 = new File(location+"file5");
-			files.add(file5);
+public class ReadXMLFile {
+    public static void main(String[] args) {
+        String location = "<add location>";
+        try {
+            //  for two or more files uncomment and add the files based on time <New to Old>
+            List<File> files = new ArrayList<>();
+	    //creating a constructor of file class and parsing an XML file
+            File file1 = new File(location + "file1.xml");
+            files.add(file1);
+            File file2 = new File(location + "file2.xml");
+            files.add(file2);
+            File file3 = new File(location + "file3.xml");
+            files.add(file3);
+            File file4 = new File(location + "file4.xml");
+            files.add(file4);
+            File file5 = new File(location + "file5.xml");
+            files.add(file5);
 
-			List<String> list = new ArrayList<>();
-			StringBuilder line = new StringBuilder();
-			
-			files.forEach(file ->
-			{
-//an instance of factory that gives a document builder
-				DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-//an instance of builder to parse the specified xml file
-				DocumentBuilder db = null;
-				try {
-					db = dbf.newDocumentBuilder();
-				} catch (ParserConfigurationException e) {
-					throw new RuntimeException(e);
-				}
-				Document doc = null;
-				try {
-					doc = db.parse(file);
-				} catch (SAXException e) {
-					throw new RuntimeException(e);
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
-				doc.getDocumentElement().normalize();
+            List<String> list = new ArrayList<>();
+            StringBuilder line = new StringBuilder();
 
-				//price
+            files.forEach(file -> {
+				//an instance of factory that gives a document builder
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+				//an instance of builder to parse the specified xml file
+                DocumentBuilder db = null;
+                try {
+                    db = dbf.newDocumentBuilder();
+                } catch (ParserConfigurationException e) {
+                    throw new RuntimeException(e);
+                }
+                Document doc = null;
+                try {
+                    doc = db.parse(file);
+                } catch (SAXException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                doc.getDocumentElement().normalize();
+
+                //price
 
 //				NodeList nodeList = doc.getElementsByTagName("PriceChangeQualification");
 //
@@ -80,36 +75,29 @@ public class ReadXMLFile
 //					}
 //				}
 
-				// inv
+                // inv
 
-				NodeList nodeList = doc.getElementsByTagName("Item");
+                NodeList nodeList = doc.getElementsByTagName("Item");
 
-				for (int itr = 0; itr < nodeList.getLength(); itr++)
-				{
-					Node node = nodeList.item(itr);
-					if (node.getNodeType() == Node.ELEMENT_NODE)
-					{
-						Element eElement = (Element) node;
-						if(!list.contains(eElement.getElementsByTagName("ItemID").item(0).getTextContent()))
-						{
-							list.add(eElement.getElementsByTagName("ItemID").item(0).getTextContent());
-							line.append(eElement.getElementsByTagName("ItemID").item(0).getTextContent()).append(",")
-									.append(eElement.getElementsByTagName("InvValue").item(0).getTextContent()).append(",")
-									.append(file.getName()).append("\n");
-						}
-					}
-				}
+                for (int itr = 0; itr < nodeList.getLength(); itr++) {
+                    Node node = nodeList.item(itr);
+                    if (node.getNodeType() == Node.ELEMENT_NODE) {
+                        Element eElement = (Element) node;
+                        if (!list.contains(eElement.getElementsByTagName("ItemID").item(0).getTextContent())) {
+                            list.add(eElement.getElementsByTagName("ItemID").item(0).getTextContent());
+                            line.append(eElement.getElementsByTagName("ItemID").item(0).getTextContent()).append(",").append(eElement.getElementsByTagName("InvValue").item(0).getTextContent()).append(",").append(file.getName()).append("\n");
+                        }
+                    }
+                }
 
-			});
+            });
 
-			File csvFile = new File(location+"inv1.csv");
-			FileWriter fileWriter = new FileWriter(csvFile);
-			fileWriter.write(line.toString());
-			fileWriter.close();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+            File csvFile = new File(location + "inv1.csv");
+            FileWriter fileWriter = new FileWriter(csvFile);
+            fileWriter.write(line.toString());
+            fileWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
